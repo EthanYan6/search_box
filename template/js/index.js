bf0c202920c02fc3b71144e657cb7381a0beb58f
Ethanyan <yanyuliang6@163.com>
2019-05-31 10:04:48 +0800

    重新设计界面布局，对接搜狗微信，为两个按钮添加功能

--- template/js/index.js
+++ template/js/index.js
@@ -1,6 +1,12 @@
 window.onload = function(){
     var btn = document.getElementsByClassName('button')[0];
+    var btn2 = document.getElementsByClassName('button')[1];
+    var value = document.getElementsByClassName('text')[0];
     btn.onclick = function(){
-        alert("你点击了搜索按钮")
+        location = 'https://weixin.sogou.com/weixin?type=2&s_from=input&query='+ value.value +'&ie=utf8'
+        
+    };
+    btn2.onclick = function(){
+        location = 'https://weixin.sogou.com/weixin?type=1&s_from=input&query='+ value.value +'&ie=utf8'
     }
 }
